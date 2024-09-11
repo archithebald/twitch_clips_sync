e7f24b9db0dbac2eea64c3c785957c2eb9a053b1
salmen1234 <salmendjeghem@gmail.com>
2024-09-11 08:56:06 +0200

    Added twitch functions

--- src/twitch.py
+++ src/twitch.py
@@ -1,11 +1,58 @@
 import requests
 import os
 
+from datetime import datetime, timedelta
+
 from dotenv import load_dotenv
 load_dotenv()
 
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    
+def get_start_date():
+    seven_days_ago = datetime.today() - timedelta(days=7)
+    formatted_date = seven_days_ago.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-def get_clips(streamer_id: str, game_id: str):
-    url = "https://api.twitch.tv/helix/clips"
+    return formatted_date
+
+def get_end_date():
+    formatted_date = datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    return formatted_date
+
+def get_access_token():
+    url = f"https://id.twitch.tv/oauth2/token?client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&grant_type=client_credentials"
+    
+    res = requests.post(url=url)
+    
+    return res.json()["access_token"]
+    
+access_token = get_access_token()
+
+def get_clips(start_date: str, end_date: str, streamer_id: str = "", game_id: str = ""):
+    url = f"https://api.twitch.tv/helix/clips?broadcaster_id={streamer_id}&started_at={start_date}&ended_at={end_date}"
+    
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Client-Id": CLIENT_ID
+    }
+    
+    res = requests.get(url=url, headers=headers)
+    
+    return res.json()["data"]
+
+def get_streamer_id(channel_name: str):
+    url = f"https://api.twitch.tv/helix/users?login={channel_name}"
+    
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Client-Id": CLIENT_ID
+    }
+    
+    res = requests.get(url=url, headers=headers)
+    
+    return res.json()["data"][0]["id"]
+
+ide = get_streamer_id("gotaga")
+
+print(get_clips(streamer_id=ide, start_date=get_start_date(), end_date=get_end_date()))
